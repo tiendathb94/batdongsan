91b4528ae6b91a231c1a865015ba14475cbb4ad9
Pham Dat <datpt.hn@havaz.vn>
2021-01-22 10:22:32 +0700

    update

--- resources/views/default/pages/project/project_detail.blade.php
+++ resources/views/default/pages/project/project_detail.blade.php
@@ -3,7 +3,28 @@
 @section('page_title')
     {{$project->category->name}} | {{$project->long_name}}
 @endsection
-
+@section('head')
+   <meta http-equiv="content-type" content="text/html; charset=utf-8">
+   <meta http-equiv="audience" content="general"><meta name="resource-type" content="document">
+   <meta name="abstract" content="Thông tin nhà đất Việt Nam">
+   <meta name="classification" content="Bất động sản Việt Nam">
+   <meta name="area" content="Nhà đất và bất động sản">
+   <meta name="placename" content="Việt Nam">
+   <meta name="author" content="Batdongsan.com.vn">
+   <meta name="copyright" content="©2007 Batdongsan.com.vn">
+   <meta name="owner" content="Batdongsan.com.vn">
+   <meta name="generator" content="Công ty Cổ phần PropertyGuru Việt Nam">
+   <meta name="distribution" content="Global">
+   <meta name="revisit-after" content="1 days">
+   <meta property="og:image" content="https://staticfile.batdongsan.com.vn/images/Logo/BDSLogo.jpg">
+   <meta name="robots" content="index,follow">
+   <meta name="keywords" content="{{$project->category->meta_keyword}}">
+   <meta name="description" content="{{$project->category->meta_description}}">
+   <meta property="og:url" content="{{route('pages.project.project_detail',['slug' => $project->slug, 'categorySlug' => $project->category->slug])}}">
+   <meta property="og:type" content="website">
+   <meta property="og:title" content="{{$project->category->meta_title}}">
+   <meta property="og:description" content="{{$project->category->meta_description}}">
+@endsection
 @section('content')
     <div class="container page-project-detail-container mt-5 mb-5">
         <div id="js-form-search"></div>
